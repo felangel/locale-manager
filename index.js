85d08883da6aa713bb605deee1ba4bae93d49fa6
felangel <felangel@umich.edu>
2016-09-10 16:38:52 -0500

    removed init method

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var glob = require('glob');
 var path = require('path');
 var localeManager = {};
+var files = glob.sync(__dirname + '/../../locales/*.js');
 
-function init() {
-	var files = glob.sync(__dirname + '/../../locales/*.js');
-
-	glob.sync(__dirname + '/../../locales/*.js').map(function (file) {
-  		var responses = require(path.resolve(file));
-  		localeManager[responses.locale] = responses;
-	});
-}
-
-init();
+glob.sync(__dirname + '/../../locales/*.js').map(function (file) {
+	var responses = require(path.resolve(file));
+	localeManager[responses.locale] = responses;
+});
 
 module.exports = localeManager;
