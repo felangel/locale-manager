418a9cd17896aa17ed33b7160c6f3e2348f6ae3a
felangel <felangel@umich.edu>
2016-09-10 16:50:41 -0500

    cleaned up even more

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var glob = require('glob');
 var path = require('path');
 var localeManager = {};
-var files = glob.sync(__dirname + '/../../locales/*.js');
 
 glob.sync(__dirname + '/../../locales/*.js').map(function (file) {
-	var responses = require(path.resolve(file));
-	localeManager[responses.locale] = responses;
+	var config = require(path.resolve(file));
+	localeManager[config.locale] = config;
 });
 
 module.exports = localeManager;
